12a1c450b314b0ecfeb4245e034559a67c2e89ba
Washington Junior <w.r_1991@hotmail.com>
2023-05-08 22:29:41 -0300

    Aula de hoje

--- CadastroCliente/views.py
+++ CadastroCliente/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    meu_nome = 'Washington Junior'
+    listas_frutas = ['Laranja', 'Maca', 'Banana']
+    context = {
+        'nome': meu_nome,
+        'frutas': listas_frutas
+    }
+    return render(request, 'index.html', context)
